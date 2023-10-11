d20f0d833b26440cdc3e4f935477311009a09998
Surya755 <suryashanmukha755@gmail.com>
2023-10-11 12:42:45 +0000

    solution

---
+++ src/main/java/com/example/goodreads/service/BookJpaService.java
@@ -0,0 +1,9 @@
+package com.example.goodreads.service;
+
+import com.example.goodreads.repository.BookRepository;
+import com.example.goodreads.model.Book;
+import java.util.*;
+
+public class BookJpaService implements BookRepository {
+    
+}
